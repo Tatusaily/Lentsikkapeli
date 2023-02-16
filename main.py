50b3051870a36cb516f360055d1aafa6e244e8b2
Tatusaily <tatu.sailynoja@gmail.com>
2023-02-16 14:43:05 +0200

    alotin

---
+++ main.py
@@ -0,0 +1,10 @@
+def newplayer:
+    #Tähän koodia joka tekee uuden pelaajan sql kantaan.
+    #Pelaajan tiedot on game-taulussa
+
+
+while gameRunning = True:
+    #jotain
+
+
+quit(Print("Nyt sammutaan."))
