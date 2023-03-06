7a6a22222112cf887f3491869688b89c5137908d
Tatusaily <tatu.sailynoja@gmail.com>
2023-03-06 15:51:04 +0200

    funktioita valmiimmaksi

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ def uusipeli():
 
     # Otetaan pelaajalta nimi ja salasana, tarkistetaan ne ja laitetaan ne kantaan
     while True:
+        global pelaajanimi
+        global salasana
         pelaajanimi = str(input("Anna pelaajanimesi: "))
         salasana = str(input("Anna salasanasi: "))
         # Tarkistetaan pelaajan nimi
         query = f"SELECT count(*) as count FROM game WHERE screen_name = '{pelaajanimi}';"
         kursori.execute(query)
         onkonimi = kursori.fetchone()
+        #TODO: Nimi/Salasana kombo
         if onkonimi[0] > 0:
             print("Pelaajanimi on jo käytössä. Yritä jotakin toista.")
         else:
@@ -40,14 +43,13 @@ def uusipeli():
     kursori.close()
     yhteys.close()
     return
-
-def jatkapeli():
-    print("JATKAPELI")
+def tunnustarkistus():
+    print("TUNNUSTARKISTUS")
     oikein = False
-    # Pelaaja syöttää nimen, jos nimi on tietokannassa niin:
-        # Pelaaja syöttää salasanan tai poistuu takaisin menuun
-    pelaajanimi = input("Anna käyttäjänimi: ")
-    salasana = input("Anna salasana: ")
+    global pelaajanimi
+    global salasana
+    pelaajanimi = str(input("Anna käyttäjänimi: "))
+    salasana = str(input("Anna salasana: "))
     yhteys = mysql.connector.connect(
         host='localhost', port=3306,
         database='flight_game', user='user1',
@@ -61,12 +63,78 @@ def jatkapeli():
             oikein = True
     # Tässä vaihessa tunnus on varmistettu
     return oikein
+def jatkapeli():
+    yhteys = mysql.connector.connect(
+        host='localhost', port=3306,
+        database='flight_game', user='user1',
+        password='sala1', autocommit=True)
+    kursori = yhteys.cursor()
+    global pelaajanimi
+    """
+    jos pelaajan sijainti on 0
+        laitetaan käyttäjä satunnaiselle kentälle randomport-taulusta
+    jos ei
+        käyttäjä jatkaa siitä missä on (game/location)
+        muistuttaa
+    """
+    query = f"SELECT * FROM GAME WHERE screen_name = '{pelaajanimi}';"
+    kursori.execute(query)
+    tulos = kursori.fetchone()
+
+    # Tehdään kaikista pelaajan tiedoista globaaleja muuttujia myöhempää varten.
+    global pelaajaid
+    pelaajaid = tulos[0]
+    global points
+    points = tulos[1]
+    global P_location
+    P_location = tulos[2]
+    pelaajanimi = tulos[3]
+    global salasana
+    salasana = tulos[4]
+    query = f"SELECT ICAO FROM randomport WHERE pelaaja_id = '{pelaajaid}';"
+    global P_kentat
+    P_kentat = []
+    kursori.execute(query)
+    kentat = kursori.fetchall()
+    for kentta in kentat:
+        P_kentat.append(kentta[0])
+
+    if P_location == 0:
+        P_location = random.sample(P_kentat, 1)
+    return
+
+
+def pelaajaliike():
+    global P_location
+    #TODO tää funktio kokonaan :(
+    #pelaaaja on jossain ja se menee johonnkin.
+    #menon jälkeen tulee kyssäri?
+    uusilocation = str(input("Anna uusi location"))
+
+    # kyssät tähän
+    if kyssäfunktio() == True:
+        points += 100
+        print("Vastaus oikein :)")
+        P_location = uusilocation
+    else:
+        points -= 1
+        print("Vastaus väärin :(")
+        P_location = random.sample(P_kentat, 1)
+    return
+
+
+def kyssäfunktio():
+
+    return
+
 
 def highscore():
     print("HIGHSCORE")
     return
 
+
 def päämenu():
+    global gamestate
     print("TERVETULOA LENTOPELIIN!\n"
           "1: Aloita uusi peli.\n"
           "2: Jatka vanhaa peliä.\n"
@@ -79,9 +147,10 @@ def päämenu():
         print(filtered_airports)
         gamestate = "jatkapeli"
     elif valinta == 2:
-        if jatkapeli() == True:
+        if tunnustarkistus() == True:
             # Tunnus on oikein ja voidaan jatkaa
             print("Oikein! :)")
+            gamestate = "jatkapeli"
         else:
             print("Tunnus väärin! :(")
 
@@ -91,6 +160,7 @@ def päämenu():
         gameRunning = False
         quit()
 
+
 def save():
     # Jos pelaajan nimi ja salasana on tietokannassa niin tallennetaan edistys
     # Jos ei löydy, niin luodaan uusi rivi
@@ -98,7 +168,8 @@ def save():
 
 
 # Vakiot
-
+pelaajanimi = ""
+salasana = ""
 # Alkaa
 gamestate = "päämenu"
 
@@ -110,4 +181,11 @@ while gameRunning == True:
     elif gamestate == "uusipeli":
         uusipeli()
     elif gamestate == "jatkapeli":
+        #TODO Tää loppuun
         jatkapeli()
+        pelaajaliike()
+
+"""        "Mitä haluut"
+        liike()
+            kyssäri
+        gamestate = "päämenu"""
