10eb7317ce0db17c255c35f62a5e538fde452962
Tatusaily <tatu.sailynoja@gmail.com>
2023-04-27 13:33:55 +0300

    Kartta näkyy ja siinä on eri moodit

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-let map = L.map("map").setView([60.224168, 24.758141], 13)
+//PÄÄOHJELMA
+// Tehdään karttaolio "map". "L" viittaa Leaflet-apiin.
+let map = L.map("map").setView([60.224168, 24.758141], 15);
+// Luodaan eri layerit joita voi vaihtaa mapin oikeesta kulmasta.
+// Näitä löytyy nimellä "tilelayer" tai "baselayer".
+const lightlayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {maxZoom: 10,})
+const darklayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {maxZoom: 10,})
+// Laitetaan layerit listaan ja linkataan se lista karttaan.
+const maplayers = {"Lightmode": lightlayer, "Darkmode": darklayer}
+const layerControl = L.control.layers(maplayers).addTo(map)
+// Aloitetaan vaalealla layerilla
+lightlayer.addTo(map)
