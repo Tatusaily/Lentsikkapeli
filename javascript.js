a15664e10cd6d25398458b20277d3dda224554dd
Tatusaily <tatu.sailynoja@gmail.com>
2023-04-27 14:33:38 +0300

    nappi näkyy kartassa

--- javascript.js
+++ javascript.js
@@ -3,10 +3,20 @@
 let map = L.map("map").setView([60.224168, 24.758141], 15);
 // Luodaan eri layerit joita voi vaihtaa mapin oikeesta kulmasta.
 // Näitä löytyy nimellä "tilelayer" tai "baselayer".
-const lightlayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {maxZoom: 10,})
-const darklayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {maxZoom: 10,})
+// Näihin pitäis copyright-säännön mukaan laittaa joku "attribution" juttu mut ei laiteta ku se näyttää rumalta.
+const lightlayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {maxZoom: 7,})
+const darklayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {maxZoom: 7,})
 // Laitetaan layerit listaan ja linkataan se lista karttaan.
 const maplayers = {"Lightmode": lightlayer, "Darkmode": darklayer}
 const layerControl = L.control.layers(maplayers).addTo(map)
 // Aloitetaan vaalealla layerilla
 lightlayer.addTo(map)
+
+
+/* testi
+laittaa merkin koordinaattiin (60, 24)
+vois testaa laittaa for -loopilla läjän näitä
+    vaikka tietokannasta?
+    tarkotus ois vetää sieltä python koodista API-kutsulla
+*/
+L.marker([60, 24]).addTo(map)
