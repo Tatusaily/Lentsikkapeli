12f38f710a1ec9acbe4064161c6082fc7abea274
Tatu <tatu.sailynoja@gmail.com>
2023-05-01 18:05:34 +0300

    Python API

---
+++ Python API.py
@@ -0,0 +1,36 @@
+import mysql.connector
+import random
+import json
+from flask import Flask
+# API rakennus
+app = Flask(__name__)
+
+
+# getplayerdata palauttaa määritellyn pelaajan tiedot tietokannasta
+@app.route('/getplayerdata/<playername>,<password>')
+def getplayerdata(playername, password):
+    sqlconnection = mysql.connector.connect(
+        host='localhost',
+        port=3306,
+        database='flight_game',
+        user='user1',
+        password='sala1',
+        autocommit=True
+    )
+    query = f"SELECT screen_name, points, location " \
+            f"FROM game " \
+            f"WHERE screen_name= '{playername}' and password= '{password}'"
+    sqlcursor = sqlconnection.cursor()
+    sqlcursor.execute(query)
+    sqlresult = sqlcursor.fetchone()
+    playerdata = {
+        "name": sqlresult[0],
+        "points": sqlresult[1],
+        "location": sqlresult[2]
+    }
+    return playerdata
+
+
+# Pääohjelma käynnistää flask-taustapalvelun localhostiin
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+existingUser = Function(){
+
+}
+
 //PÄÄOHJELMA
 // Tehdään karttaolio "map". "L" viittaa Leaflet-apiin.
 let map = L.map("map").setView([60.224168, 24.758141], 15);
