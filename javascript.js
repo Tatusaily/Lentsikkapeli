e7a79f0188fb465a2ae39c84790c8dd788cacc63
Tatusaily <tatu.sailynoja@gmail.com>
2023-05-03 18:31:39 +0300

    liikkumisen alku

--- javascript.js
+++ javascript.js
@@ -1,23 +1,15 @@
-existingUser = async function(){
+// Funktiot
+existingUser = async function(name, pass){
+    /*
     const name = document.getElementById("username").value
     const pass = document.getElementById("password").value
-    /*
-    const proxy = 'https://users.metropolia.fi/~ilkkamtk/proxy.php?url='
-    const apiurl = `${proxy}http://127.0.0.1:3000/getplayerdata/${name},${pass}`
      */
-
-    // tulee joku cors error
-    // toimii oikein jos laittaa apirulin suoraan selaimen URLiin.
     const apiurl = `http://127.0.0.1:3000/getplayerdata/${name},${pass}`
-    console.log(apiurl)
-    const apiresponse = await fetch(apiurl)
-    const apiresponse2 = await apiresponse.json()
-    console.log(apiresponse2)
-    return apiresponse2
+    let apiresponse = await fetch(apiurl)
+    apiresponse = await apiresponse.json()
+    console.log(apiresponse)
 }
-
-// Yksi funktio jolla voi kutsua eri kysymykset. Varmaan helpoin käyttää tätä napeissa
-getQuestion = async function(category){
+getQuestion = async function(category)  {
     // muutetaan category kutsuttavaan muotoon
     switch (category){
         case "History":
@@ -65,34 +57,6 @@ getQuestion = async function(category){
     }
 
 }
-/*
-getGeneralQuestion = async function(){
-    // otetaan kyssä
-    currentCategory = "general"
-    const randomQuestion = await generalTrivia()
-    const question = randomQuestion.results[0].question
-    const correct = randomQuestion.results[0].correct_answer
-    let answers = randomQuestion.results[0].incorrect_answers
-    answers.push(correct)
-    rightanswer = correct
-    answers = answers.sort(() => 0.5 - Math.random())
-    // DEBUG LOG
-        console.log(question)
-        console.log(answers)
-        console.log(correct)
-        console.log(randomQuestion)
-}
-getMainQuestion = async function(category){
-    // otetaan kyssä
-    const mainQuestion = await mainTrivia(category)
-    const questionArray = mainQuestion.results[0]
-    const questionText = questionArray.question
-    const correct = questionArray.correct_answer
-    let answers = questionArray.incorrect_answers
-    answers.push(correct)
-    rightanswer = correct
-    answers = answers.sort(() => 0.5 - Math.random())
-}*/
 checkAnswer = async function(answer){
     let isCorrect
     if (answer === rightanswer){
@@ -112,13 +76,19 @@ getEuropeAirports = async function(){
 }
 drawEuropeAirports = async function(){
     const euAirportList = await getEuropeAirports()
-    console.log("saatiin")
+    console.log("saatiin kentät, aletaan piirtämään")
     for (let airport of euAirportList){
+        const button = document.createElement("button")
+        button.innerHTML = "Fly to airport"
+        button.onclick = function(){flyToAirport(button[2])}
         let marker = L.marker([airport[0], airport[1]], {icon: myIcon}).addTo(map)
-        let popupContent = `Name: ${airport[3]}, ICAO: ${airport[2]}`
+        let popupContent = `Name: ${airport[3]}, ICAO: ${airport[2], button}`
         marker.bindPopup(popupContent).openPopup()
     }
-    console.log("tehty")
+    console.log("kentät piirretty :)")
+}
+flyToAirport = async function(ICAO){
+
 }
 
 
@@ -126,6 +96,8 @@ drawEuropeAirports = async function(){
 
 //---------------PÄÄOHJELMA----------------
 //---TÄÄ SUORITETAAN AINA KUN HTML AUKEE---
+// Alustetaan Muutujia
+let player = ""
 let points = 10000
 document.getElementById("points").innerText = points
 let rightanswer = ""
@@ -175,14 +147,3 @@ const myIcon = L.icon({
     iconUrl: 'IMG/star.png',
     iconSize: [10, 10]
 })
-// Piirretään EU kentät kartalle
-//drawEuropeAirports()
-
-
-/* testi
-laittaa merkin koordinaattiin (60, 24)
-vois testaa laittaa for -loopilla läjän näitä
-    vaikka tietokannasta?
-    tarkotus ois vetää sieltä python koodista API-kutsulla
-*/
-//L.marker([60, 24]).addTo(map)
